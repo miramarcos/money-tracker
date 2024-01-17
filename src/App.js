d12a8e6d1b4b49bd9e67b5cdaff113e7a2e8e595
Mira Marcos <mirakrismarcos@gmail.com>
2024-01-17 12:55:57 +0800

    Show All Transactions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [name, setName] = useState('');
   const [datetime, setDateTime] = useState('');
   const [description, setDescription] = useState('');
+  const [transactions, setTransactions] = useState('');
+  
+  useEffect(() => {
+    getTransactions().then(setTransactions())
+  },[])
+  
+  const getTransactions = async () => {
+    const url = process.env.REACT_APP_API_URL + '/transactions';
+    const response = await fetch(url);
+    return await response.json();
+  }
   const addNewTransaction = (ev) => {
     ev.preventDefault();
     const url = process.env.REACT_APP_API_URL + '/transaction';
@@ -46,39 +57,23 @@ function App() {
             onChange={ev => setDescription(ev.target.value)}
             placeholder={"description"} />
         </div>
-        <button type = "submit">Add New Transaction</button>
+        <button type="submit">Add New Transaction</button>
       </form>
-      <div className="transactions">
-        <div className="transaction">
+      <div className="transactions_container">
+        {transactions.length > 0 && transactions.map(transaction => (
+          <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
-            <div className="description">It was time for new TV</div>
+            <div className="name">{transaction.name}</div>
+            <div className="description">{transaction.description}</div>
           </div>
           <div className="right">
-            <div className="price red">- $500</div>
-            <div className="datetime">2023-12-1 15:45</div>
+              <div className={"price" + (transaction.price < 0) ? 'red' : 'green'}>
+                {transaction.price}
+              </div>
+            <div className="datetime">{transaction.datetime}</div>
           </div>
         </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Gig Job new website</div>
-            <div className="description">Got a new gig income</div>
-          </div>
-          <div className="right">
-            <div className="price green">+ $400</div>
-            <div className="datetime">2023-12-1 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Iphone</div>
-            <div className="description">It was time for new iPhone</div>
-          </div>
-          <div className="right">
-            <div className="price red">- $900</div>
-            <div className="datetime">2023-12-1 15:45</div>
-          </div>
-        </div>
+        ))}
       </div>
    </main>
   );
